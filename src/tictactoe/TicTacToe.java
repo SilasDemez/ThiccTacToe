1d2efffecb026a02a88d2ee8006e5ae603e06262
Silas Demez <silasdemez@gmail.com>
2021-03-24 10:14:04 +0100

    made resizable

--- src/tictactoe/TicTacToe.java
+++ src/tictactoe/TicTacToe.java
@@ -5,6 +5,7 @@ import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.image.Image;
 import javafx.stage.Stage;
 
 public class TicTacToe extends Application {
@@ -14,10 +15,10 @@ public class TicTacToe extends Application {
         Parent root = FXMLLoader.load(getClass()
                 .getResource("tictactoe.fxml"));
         primaryStage.setTitle("ThiccTacToe");
-        Scene scene = new Scene(root, 300, 275);
+        Scene scene = new Scene(root, 300, 325);
         scene.getStylesheets().add(getClass()
                 .getResource("tictactoe.css").toExternalForm());
-        primaryStage.setResizable(false);
+        primaryStage.getIcons().add(new Image("file:C:\\Users\\user\\Downloads\\gamification.png"));
         primaryStage.setScene(scene);
         primaryStage.show();
     }
